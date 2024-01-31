372ead74df57e281c229bcbc099edfd3c75eda78
Anubhav ray <anubhavray678@gmail.com>
2024-01-31 21:36:59 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,37 @@ import Link from "next/link";
 import Lottie from "lottie-react";
 import animationData from "../../public/a2.json";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import styles from "./home.module.css";
 export default function Home() {
   return (
     <main>
-      <section>
-        <div className="flex flex-col w-full justify-center items-center h-2/5">
-          <Lottie
-            animationData={animationData}
-            className="flex justify-center items-center"
-            loop={true}
-          />
+      <section className="flex flex-col w-full justify-center items-center">
+        <div className={styles.container}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+            <p className={styles.desc}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+              blanditiis adipisci minima reiciendis a autem assumenda dolore.
+            </p>
+            <div className={styles.buttons}>
+              <Button className={styles.button}>Learn More</Button>
+              <Button className={styles.button}>Contact</Button>
+            </div>
+            {/* <div className={styles.brands}>
+              <Image
+                src="/brands.png"
+                alt=""
+                fill
+                className={styles.brandImg}
+              />
+            </div> */}
+          </div>
+          <div className={styles.imgContainer}>
+            <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+          </div>
         </div>
       </section>
-      {/* <Button>Button</Button> */}
     </main>
   );
 }
