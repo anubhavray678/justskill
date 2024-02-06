62c6eefea6b6515e770c693bdf5de95fec57a664
fastbricks <anubhavray678@gmail.com>
2024-02-06 18:18:53 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,25 @@
-"use client";
-import { useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Link,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-} from "react-scroll";
-import Image from "next/image";
-import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
-import HeadTag from "./Head";
+import FeaturedSection from "@/components/Featured/FeaturedSection";
 import Head from "next/head";
-
+export const metadata = {
+  title: "JustSkills | Improve then prove.",
+  description:
+    "JustSkills is one of the leading platforms to learn Computer Science skills. Master essential data structures and algorithms,read trending tech and expand your knowledge on different tech stuff.",
+  url: "https://justskills.in/",
+  openGraph: {
+    title: "JustSkills | Improve then prove.",
+    description:
+      "JustSkills is one of the leading platforms to learn Computer Science skills. Master essential data structures and algorithms,read trending tech and expand your knowledge on different tech stuff.",
+    url: "https://justskills.in/",
+    image:
+      "https://justskills.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.bd81bc90.png&w=640&q=75",
+  },
+};
 export default function Home() {
-  useEffect(() => {
-    // Registering the 'begin' event and logging it to the console when triggered.
-    Events.scrollEvent.register("begin", (to, element) => {
-      console.log("begin", to, element);
-    });
-
-    // Registering the 'end' event and logging it to the console when triggered.
-    Events.scrollEvent.register("end", (to, element) => {
-      console.log("end", to, element);
-    });
-
-    // Updating scrollSpy when the component mounts.
-    scrollSpy.update();
-
-    // Returning a cleanup function to remove the registered events when the component unmounts.
-    return () => {
-      Events.scrollEvent.remove("begin");
-      Events.scrollEvent.remove("end");
-    };
-  }, []);
-
-  // Function to handle the activation of a link.
-  const handleSetActive = (to) => {
-    console.log(to);
-  };
-
   return (
     <>
       <Head>
         <title>JustSkills | Improve then prove.</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="JustSkills" />
         <meta
           name="description"
           content="JustSkills is one of the leading platforms to learn Computer Science skills. Master essential data structures and algorithms,read trending tech and expand your knowledge on different tech stuff."
@@ -62,50 +36,7 @@ export default function Home() {
         <meta property="og:url" content="https://justskills.in/" />
       </Head>
       <main>
-        <section className="">
-          <div className=" w-full justify-items-center md:flex justify-center md:justify-between">
-            <div className="w-[100%] md:w-[30%] relative z-50 justify-evenly flex flex-col">
-              <div>
-                <h1 className=" font-extrabold text-6xl sm:text-8xl text-primary">
-                  Creative,
-                </h1>
-                <h1 className=" font-extrabold text-6xl sm:text-8xl text-primary">
-                  Thoughts,
-                </h1>
-                <h1 className=" font-extrabold text-6xl sm:text-8xl text-primary">
-                  Age,
-                </h1>
-              </div>
-              <div className="">
-                <p className="font-bold text-2xl sm:text-4xl text-primary pt-10">
-                  Improve then prove
-                </p>
-                <div className="">
-                  <Link
-                    activeClass="active"
-                    to="test1"
-                    spy={true}
-                    smooth={true}
-                    offset={50}
-                    duration={500}
-                    onSetActive={handleSetActive}
-                  >
-                    <Button className="mt-10 w-[50%] gap-2 p-6">
-                      Explore
-                      <KeyboardDoubleArrowDownIcon />
-                    </Button>{" "}
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className=" w-[100%] relative h-[60vh] gap-10 md:w-[60%] md:h-[90vh]">
-              <Image src="/hero.gif" alt="" fill className=" pt-8 rounded" />
-            </div>
-          </div>
-          <div id="myDiv" className=" h-[200px]">
-            <Element name="test1" className="element"></Element>
-          </div>
-        </section>
+        <FeaturedSection />
       </main>
     </>
   );
---
+++ src/components/Featured/FeaturedSection.jsx
@@ -0,0 +1,85 @@
+"use client";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from "react-scroll";
+import Image from "next/image";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+
+export default function FeaturedSection() {
+  useEffect(() => {
+    // Registering the 'begin' event and logging it to the console when triggered.
+    Events.scrollEvent.register("begin", (to, element) => {
+      console.log("begin", to, element);
+    });
+
+    // Registering the 'end' event and logging it to the console when triggered.
+    Events.scrollEvent.register("end", (to, element) => {
+      console.log("end", to, element);
+    });
+
+    // Updating scrollSpy when the component mounts.
+    scrollSpy.update();
+
+    // Returning a cleanup function to remove the registered events when the component unmounts.
+    return () => {
+      Events.scrollEvent.remove("begin");
+      Events.scrollEvent.remove("end");
+    };
+  }, []);
+  const handleSetActive = (to) => {
+    console.log(to);
+  };
+
+  return (
+    <section className="">
+      <div className=" w-full justify-items-center md:flex justify-center md:justify-between">
+        <div className="w-[100%] md:w-[30%] relative z-50 justify-evenly flex flex-col">
+          <div>
+            <h1 className=" font-extrabold text-6xl sm:text-8xl text-primary">
+              Creative,
+            </h1>
+            <h1 className=" font-extrabold text-6xl sm:text-8xl text-primary">
+              Thoughts,
+            </h1>
+            <h1 className=" font-extrabold text-6xl sm:text-8xl text-primary">
+              Age,
+            </h1>
+          </div>
+          <div className="">
+            <p className="font-bold text-2xl sm:text-4xl text-primary pt-10">
+              Improve then prove
+            </p>
+            <div className="">
+              <Link
+                activeClass="active"
+                to="test1"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+                onSetActive={handleSetActive}
+              >
+                <Button className="mt-10 w-[50%] gap-2 p-6">
+                  Explore
+                  <KeyboardDoubleArrowDownIcon />
+                </Button>{" "}
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className=" w-[100%] relative h-[60vh] gap-10 md:w-[60%] md:h-[90vh]">
+          <Image src="/hero.gif" alt="" fill className=" pt-8 rounded" />
+        </div>
+      </div>
+      <div id="myDiv" className=" h-[200px]">
+        <Element name="test1" className="element"></Element>
+      </div>
+    </section>
+  );
+}
