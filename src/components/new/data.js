4e91eb2d825ffef21503618b142fce74daa8f3ec
fastbricks <64881373+anubhavray678@users.noreply.github.com>
2024-06-30 16:57:51 +0530

    Update data.js

--- src/components/new/data.js
+++ src/components/new/data.js
@@ -11,13 +11,13 @@ import benefitOneImg from "../../../public/img/benefit-one.png";
 import benefitTwoImg from "../../../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
+  title: "Course highlight and benefits",
   desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
   image: benefitOneImg,
   bullets: [
     {
       title: "Understand your customers",
-      desc: "Then explain the first point breifly in one or two lines.",
+      desc: "Then explain the first point briefly in one or two lines.",
       icon: <FaceSmileIcon />,
     },
     {
